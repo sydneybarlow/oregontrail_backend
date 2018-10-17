fbd87b17705ec086f22909e33847b2c48684ed53
github username <github email address>
2018-10-17 14:01:03 -0400

    fixed serializer for money and miles

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :password
+  attributes :id, :name, :username, :password, :money, :miles
   has_many :family_members
   has_many :supplies
 end
