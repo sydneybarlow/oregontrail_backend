ac68c8c400592dfc41cbcbfdf3e926194a2d701c
github username <github email address>
2018-10-24 16:39:47 -0400

    Fixed seed data error

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 syd = User.create(id: 1, name: 'Sydney', username: 'sydbid', password: '123', money: 1000, miles: 1795, days: 0)
 
-jed = FamilyMember.create(id: 1, name: 'Jedediah', health: 'good', status: 'alive', user_id: syd.id, role: 'pops')
-jacob = FamilyMember.create(id: 2, name: 'Jacob', health: 'good', status: 'alive', user_id: syd.id, role: 'boy')
-mary = FamilyMember.create(id: 3, name: 'Mary', health: 'good', status: 'alive', user_id: syd.id, role: 'momma')
-jess = FamilyMember.create(id: 4, name: 'Jess', health: 'good', status: 'alive', user_id: syd.id, role: 'girl')
+jed = FamilyMember.create(name: 'Jedediah', health: 'good', status: 'alive', user_id: syd.id, role: 'pops')
+jacob = FamilyMember.create(name: 'Jacob', health: 'good', status: 'alive', user_id: syd.id, role: 'boy')
+mary = FamilyMember.create(name: 'Mary', health: 'good', status: 'alive', user_id: syd.id, role: 'momma')
+jess = FamilyMember.create(name: 'Jess', health: 'good', status: 'alive', user_id: syd.id, role: 'girl')
 
-food = Supply.create(id: 1, name: 'food', amount: 3000, user_id: syd.id)
-bullets = Supply.create(id: 2, name: 'bullets', amount: 500, user_id: syd.id)
+food = Supply.create(name: 'food', amount: 3000, user_id: syd.id)
+bullets = Supply.create(name: 'bullets', amount: 500, user_id: syd.id)
