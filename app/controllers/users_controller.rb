a7ee278e8251794230cd64e7e8bd3629a81e5f9c
github username <github email address>
2018-10-24 12:42:26 -0400

    Fixed login and user events bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      payload = { user_id: @user.id }
+      token = encode(payload)
+      render json: { user: UserSerializer.new(@user), token: token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
